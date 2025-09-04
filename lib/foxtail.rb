f3fa4ddee76818c6978d076ef8c7bcc980b9dcff
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2025-09-04 13:44:47 +0900

    :computer: bundle gem foxtail

---
+++ lib/foxtail.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "foxtail/version"
+
+module Foxtail
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/foxtail/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Foxtail
+  VERSION = "0.1.0"
+end
