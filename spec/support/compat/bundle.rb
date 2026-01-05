34bf10855c3b81746db125952bd3cf8805d122d8
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-01-05 10:35:33 +0900

    :recycle: Merge test_helper contents into main compat modules
    - Move constants and methods from test_helper.rb into syntax.rb and bundle.rb
    - Remove redundant FluentCompatBase:: prefix for inherited constants
    - Delete now-empty test_helper.rb files

--- spec/support/compat/bundle.rb
+++ spec/support/compat/bundle.rb
@@ -1,12 +1,29 @@
 # frozen_string_literal: true
 
 require_relative "base"
+require_relative "bundle/ast_converter"
 
 # Compatibility testing support for fluent-bundle
 module FluentCompatBundle
   include FluentCompatBase
   extend FluentCompatBase
-end
 
-require_relative "bundle/ast_converter"
-require_relative "bundle/test_helper"
+  SYNTAX_FIXTURES = FLUENT_JS_ROOT / "fluent-syntax" / "test" / "fixtures_structure"
+  private_constant :SYNTAX_FIXTURES
+
+  BUNDLE_FIXTURES = FLUENT_JS_ROOT / "fluent-bundle" / "test" / "fixtures_structure"
+  private_constant :BUNDLE_FIXTURES
+
+  module_function def all_fixtures
+    collect_fixtures({json_dir: BUNDLE_FIXTURES, ftl_dir: SYNTAX_FIXTURES})
+  end
+
+  def parse_ftl(source)
+    parser = Foxtail::Bundle::Parser.new
+    parser.parse(source)
+  end
+
+  def convert_to_json(entries)
+    ASTConverter.to_json_format(entries)
+  end
+end
