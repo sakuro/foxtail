fd775650304a013cc26ec6ee148cbd18a56234c7
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-01-13 10:02:19 +0900

    :recycle: Align foxtail-runtime entrypoints with foxtail-tools
    - Move loader setup to foxtail-runtime entrypoint
    - Make foxtail.rb a thin require alias

--- foxtail-runtime/lib/foxtail.rb
+++ foxtail-runtime/lib/foxtail.rb
@@ -1,23 +1,3 @@
 # frozen_string_literal: true
 
-require "zeitwerk"
-require_relative "foxtail/runtime/version"
-
-# Ruby implementation of Project Fluent localization system
-module Foxtail
-  # Configure Zeitwerk loader for this gem
-  loader = Zeitwerk::Loader.for_gem
-
-  # Ignore version.rb since it's required by gemspec before Zeitwerk loads
-  loader.ignore(__dir__ + "/foxtail/runtime/version.rb")
-  # Ignore gem entrypoint file (no constant defined)
-  loader.ignore(__dir__ + "/foxtail-runtime.rb")
-
-  # Configure inflections for acronyms
-  loader.inflector.inflect(
-    "ast" => "AST",
-    "icu4x_cache" => "ICU4XCache"
-  )
-
-  loader.setup
-end
+require "foxtail-runtime"
